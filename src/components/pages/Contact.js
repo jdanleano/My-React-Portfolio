6cdd4db2e7b9f94973e3458970a8b5c15c082191
Ysfboi <jdanleano@gmail.com>
2022-10-21 13:18:34 -0400

    Basic framework of portfolio

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Adding my contact information and links here.</p>
+    </div>
+  );
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Joseph Leano</h1>
+      <p>This is my portfolio powered by React!</p>
+    </div>
+  );
+}
